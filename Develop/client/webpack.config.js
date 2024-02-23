b612e6247d8db4493b19dfc4b44cd387e5def15e
scottd988 <>
2024-02-23 16:53:46 -0500

    database put and get

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -1,11 +1,12 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
-const InjectManifest = require('workbox-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
+
 
 module.exports = () => {
   return {
@@ -24,7 +25,7 @@ module.exports = () => {
         title: 'Webpack Plugin',
       }),
       new MiniCssExtractPlugin(),
-      new InjectManifest.GenerateSW()
+      new WorkboxPlugin.GenerateSW()
     ],
 
     module: {
